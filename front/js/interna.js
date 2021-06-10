6aa1715514ac7b865cf383d5d767a1a493c54c5d
FelipeMaciel12 <fema.bsi@gmail.com>
2021-06-09 23:04:05 -0300

    Projeto Gama v3 Criacao do Front vzero

--- front/js/interna.js
+++ front/js/interna.js
@@ -2,7 +2,7 @@
 function validaLogin() {
     let userTxt = localStorage.getItem("userLogged")
 
-    if(!userTxt) {
+    if (!userTxt) {
         window.location = "index.html"
     }
 
@@ -18,19 +18,23 @@ function logout() {
     window.location = "index.html"
 }
 
+function paginaRel() {
+    window.location = "interna.html"
+}
+
 function listarUser() {
-    let url = `http://localhost:8080/user/all` 
+    let url = `http://localhost:8080/user/all`
 
     fetch(url)
-    .then(res => res.json())
-    .then(res => exibirLista(res))
+        .then(res => res.json())
+        .then(res => exibirLista(res))
 }
 
 
 function exibirLista(lista) {
     let tabela = "<table> <tr> <th>Nome</th> <th>email</th> </tr>"
 
-    for(i=0; i < lista.length ; i++) {
+    for (i = 0; i < lista.length; i++) {
         tabela += `<tr> <td>${lista[i].nome}</td> <td>${lista[i].email}</td> </tr>`
     }
 
@@ -39,23 +43,34 @@ function exibirLista(lista) {
 }
 
 
-function listaAlarme() {
+function relAlarme() {
     window.location = "alarmes.html"
-    let url = `http://localhost:8080/alarme/todos` 
-    fetch(url)
-    .then(res => res.json())
-    .then(res => exibirAlarme(res))
-    
+
+
 }
 
-function exibirAlarme(lista){
+function relEvento() {
+    window.location = "evento.html"
+
+
+}
+
+function name() {
+    let url = `http://localhost:8080/alarme/todos`
+    fetch(url)
+        .then(res => res.json())
+        .then(res => exibirAlarme(res))
+}
+
+
+function exibirAlarme(lista) {
     let alarme = "<table> <tr> <th>IdAlarme</th> <th>nome</th> <th>descricao</th> </tr>"
 
-    for(i=0; i < lista.length ; i++) {
+    for (i = 0; i < lista.length; i++) {
         alarme += `<tr> <td>${lista[i].idAlarme}</td> <td>${lista[i].nome}</td> <td>${lista[i].descricao}</td> </tr>`
     }
 
     alarme += "</table>"
     document.getElementById("alarme").innerHTML = alarme
-    
+
 }
